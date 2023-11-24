2e4a2c59b44601fead2c4c983b79f016f3fa91a3
Rakesh <rakeshbhaskaran07@gmail.com>
2023-11-24 11:39:34 +0530

    First commit

---
+++ src/test/java/Sample/TC001Test.java
@@ -0,0 +1,16 @@
+package Sample;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class TC001Test {
+	@Test
+	public void demo1() {
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://demowebshop.tricentis.com/");
+		driver.quit();
+	}
+
+}
---
+++ src/test/java/Sample/TC002Test.java
@@ -0,0 +1,16 @@
+package Sample;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class TC002Test {
+	@Test
+	public void demo2() {
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.shoppersstack.com/");
+		driver.quit();
+	}
+
+}
